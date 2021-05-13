9d9c38162edadeb9bed36e536c448a90293a5ee3
droal <droal.tech@gmail.com>
2021-05-13 15:21:14 -0500

    Rest Api usando fetch

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 import { View, Text, Pressable, StyleSheet } from 'react-native'
+import Http from '../../libs/http'
 
 class CoinsScreen extends Component{
+
+    componentDidMount = async() => {
+        const coins = await Http.instance.get("https://api.coinlore.net/api/tickers/")
+        console.log("Get coins : ", coins)
+    }
 
     handlePress = () => {
         console.log("ir a detail", this.props)
