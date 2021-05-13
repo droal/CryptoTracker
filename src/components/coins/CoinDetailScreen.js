00fc249e9a7ce210b927fd2dfe0db46afef447a5
droal <droal.tech@gmail.com>
2021-05-13 10:55:36 -0500

    Navegación de una pantalla otra mediante el prop navigation

---
+++ src/components/coins/CoinDetailScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import CoinsScreen from './CoinsScreen'
+
+
+class CoinDetailScreen extends Component{
+
+    render(){
+        return(
+            <View>
+                <Text>CoinDetailScreen</Text>
+            </View>
+        )
+    }
+}
+
+export default CoinDetailScreen
--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, StyleSheet } from 'react-native'
 
 class CoinsScreen extends Component{
+
+    handlePress = () => {
+        console.log("ir a detail", this.props)
+        this.props.navigation.navigate('CoinDetail')
+    }
+
     render(){
         return(
-            <View>
-                <Text>qwerty</Text>
+            <View style={styles.container}>
+                <Text style={styles.titleText}>qwerty</Text>
+                <Pressable style={styles.btn} onPress={this.handlePress}> 
+                    <Text style={styles.btnText}>Ir a detalle</Text>
+                </Pressable>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: "red"
+    },
+    titleText:{
+        color:"#fff",
+        textAlign:"center"
+    },
+    btn:{
+        padding: 8,
+        backgroundColor: "blue",
+        borderRadius: 8,
+        margin:16
+    },
+    btnText:{
+        color:"#fff",
+        textAlign: "center",
+         
+    }
+})
 
 export default CoinsScreen
